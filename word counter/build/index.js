9e01d742306d7d4ecf9ac3042add7957427bdc14
huzaifasiddiqui <siddiquihuzaifa68@gmail.com>
2023-01-10 22:28:11 +0500

    Word Counter

---
+++ word counter/build/index.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+const stopIntro = async () => {
+    return new Promise((res) => {
+        setTimeout(res, 3000);
+    });
+};
+const intro = async () => {
+    const introCal = chalkAnimation.rainbow("Welcome to Huzaifa Word Counter App");
+    await stopIntro();
+    introCal.stop();
+};
+await intro();
+const answer = await inquirer.prompt({
+    type: "input",
+    name: "words",
+    message: "Enter Your Sentence! \n",
+});
+const { words } = answer;
+if (words) {
+    const splitting = words.trim().split(" ");
+    const result = splitting.length;
+    console.log(chalk.greenBright("Total Number Of Words In Your Sentence Is ") +
+        chalk.redBright(result));
+}
+else {
+    console.log(chalk.red("Please Enter Your Sentence!"));
+}

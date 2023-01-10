abb479f71184e115647ac7713ec8efde1dc698f8
huzaifasiddiqui <siddiquihuzaifa68@gmail.com>
2023-01-10 18:10:39 +0500

    Currency Converter

---
+++ Currency Converter/build/index.js
@@ -0,0 +1,59 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+const conversion = {
+    PKR: {
+        USD: 0.0044,
+        EURO: 0.0041,
+        PKR: 1,
+    },
+    USD: {
+        PKR: 227.17,
+        EURO: 0.93,
+        USD: 1,
+    },
+    EURO: {
+        PKR: 243.79,
+        USD: 1.07,
+        EURO: 1,
+    },
+};
+const stopIntro = async () => {
+    return new Promise((res) => {
+        setTimeout(res, 3000);
+    });
+};
+const intro = async () => {
+    const introCal = chalkAnimation.rainbow("Welcome to Huzaifa Currency Converter App");
+    await stopIntro();
+    introCal.stop();
+};
+await intro();
+const answer = await inquirer.prompt([
+    {
+        type: "list",
+        name: "from",
+        choices: ["PKR", "EURO", "USD"],
+        message: "Select Your Current Currency! ",
+    },
+    {
+        type: "list",
+        name: "to",
+        choices: ["PKR", "EURO", "USD"],
+        message: "Select Conversion Currency! ",
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter Amount You Want To Convert! ",
+    },
+]);
+const { from, to, amount } = answer;
+if (from && to && amount) {
+    const result = conversion[from][to] * amount;
+    console.log(chalk.green(`Your Converted Value from ${from} to ${to} is ${result}`));
+}
+else {
+    console.log(chalk.redBright("Invalid Value"));
+}
